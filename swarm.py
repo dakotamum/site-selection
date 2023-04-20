4de4fd60f08238eaf31db9cbc48847123f39f45d
clbHrrs <caleb0harris@gmail.com>
2023-04-20 13:48:26 -0600

    Updated swarm class to generate agents and sites, as well as the original implementation of the simulate function

--- swarm.py
+++ swarm.py
@@ -1,3 +1,8 @@
+from random import randint, sample
+from vacuum import *
+from Site import *
+import enums.constants
+
 """
     Class for the swarm containing all the agents.
     Attributes
@@ -14,15 +19,28 @@ class Swarm:
     agents = []
     sites = []
 
-    def __init__(self, agentCount, siteCount, sitePattern, siteFeatures):
+    def __init__(self, agentCount, siteCount, sitePattern, target):
         self.generate_agents(agentCount)
-        self.generate_sites(siteCount, sitePattern, siteFeatures)
+        self.generate_sites(siteCount, sitePattern, target)
 
     def generate_agents(self, agentCount):
-        pass
+        for i in range(agentCount):
+            self.agents.append(vacuum([randint(0, 20), randint(0, 20)], enums.constants.BLACK))
 
-    def generate_sites(self, siteCount, sitePattern, siteFeatures):
-        pass
+    def generate_sites(self, siteCount, sitePattern, target):
+        self.sites.append(Site(sitePattern, target))
+        for i in range(siteCount - 1):
+            # Generate 3 random numbers that sum to 100
+            dividers = sorted(sample(range(1, 100), 2))
+            feat1, feat2, feat3 = (a - b for a, b in zip(dividers + [100], [0] + dividers))
+            self.sites.append(Site(sitePattern, [feat1, feat2, feat3]))
+
+    def simulate(self, timesteps):
+        for site in self.sites:
+            # TODO the agents will need to be given the new site and new starting coordinates here
+            for step in range(timesteps):
+                for agent in self.agents:
+                    agent.random_move()
 
     def do_dowdall_vote(self):
         voteTotals = [0] * len(self.sites)
