fadb333c5976138161a8f32421787b831b1bb49d
clbHrrs <clb.hrrs@aggiemail.usu.edu>
2023-04-25 19:50:02 -0600

    added get_votes to drone

--- main.py
+++ main.py
@@ -16,10 +16,18 @@ def main():
         """"""
 
 def runGame():
-    swarm1 = Swarm(agentCount=CONFIG['num_drones'], siteCount=CONFIG['num_sites'], sitePattern=0, target=[33,34,33])
+    swarm1 = Swarm(agentCount=CONFIG['num_drones'], siteCount=CONFIG['num_sites'], sitePattern=0, target=CONFIG['target_color_ratio'])
     swarm1.simulate(CONFIG)
     np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
-    print(swarm1.agents[0].stored_beliefs)
+    #print(swarm1.agents[0].stored_beliefs)
+    vote_totals = swarm1.do_borda_vote()
+    print('Borda Results:')
+    for index, vote in enumerate(vote_totals):
+        print('Site ' + str(index) + ': ' + str(vote))
+    vote_totals = swarm1.do_dowdall_vote()
+    print('Dowdall Results')
+    for index, vote in enumerate(vote_totals):
+        print('Site ' + str(index) + ': ' + str(vote))
     return False
 
 if __name__ == '__main__':
